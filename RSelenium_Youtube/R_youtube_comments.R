93282b31349a24a7ae5e60cd77feabb0aabfd830
AIRIM <52808028+Leeyua-airim@users.noreply.github.com>
2019-08-06 09:35:14 +0900

    Update and rename youtube_commentsR.R to R_youtube_comments.R

--- RSelenium_Youtube/R_youtube_comments.R
+++ RSelenium_Youtube/R_youtube_comments.R
@@ -6,6 +6,10 @@ remD <- remoteDriver(remoteServerAddr = 'localhost',
 remD$open() #서버에 연결
 remD$navigate("https://www.youtube.com/watch?v=-4Hi0QDEIk4") #이 홈페이지로 이동 
 
+#영상 플레이
+btn <- remD$findElement(using="css selector", value='.html5-main-video')
+btn$clickElement()
+
 #홈페이지 스크롤
 remD$executeScript("window.scrollTo(0,500)")
 remD$executeScript("window.scrollTo(500,1000)")
@@ -17,12 +21,12 @@ html <- remD$getPageSource()[[1]]
 html <- read_html(html) #페이지의 소스 읽어오기 
 
 ytube_comments <- html %>% html_nodes("#content-text") %>% html_text() 
-ytube_comments[1:10]
+ytube_comments<-ytube_comments[1:50]
 
 ytube_comments<-gsub("\n","",ytube_comments) #데이터 정제 1
 ytube_comments<-trimws(ytube_comments)
 
 ytube_comments
 
-write.table(ytube_comments, file = "E:/R/R셀레니움/결과물/you_comments.txt",sep=",",row.names=FALSE,
+write.table(ytube_comments, file = "E:/R/R셀레니움/결과물/youtube_comments.txt",sep=",",row.names=FALSE,
             quote = FALSE)
