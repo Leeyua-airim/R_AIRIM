231bb897a6f19de1cc00a7ef6834931dc7a1a70a
Leeyua-airim <brink9205@gmail.com>
2019-12-05 14:14:47 +0900

    R_Business_Intelligence_01

---
+++ R_Business/Ch_01_ETR.R
@@ -0,0 +1,48 @@
+#R_비즈니스 인텔리전스_
+# 추출 변형 저장 _ ETL 과정
+
+getwd()
+setwd("E:/R_Restaurant")
+kickboard<-read.csv("Ch1_kickboard_sharing_data.csv")
+str(kickboard)
+
+kickboard<-read.table("Ch1_kickboard_sharing_data.txt", sep = "\t",header = TRUE)
+head(kickboard)
+
+library(dplyr)
+#봄과 여름에 한하여 미등록자의 킥보드 대여 횟수 살펴보기
+extracted_row<-filter(kickboard, 정기회원 == 0, 계절 == 1 | 계절 == 2)
+dim(extracted_row)
+
+# %in% 연산자는 모든 행을 조회하고, 그 행이 사용자가 지정한 기준에 부합하는지 결정한다.
+#첫번째 파라미터는 데이터 이름, 두번째 파라미터는 필터링 조건 
+extracted_row_in<-filter(kickboard, 정기회원 == 0, 계절 %in% c(1,2))
+#identical() 함수는 R의 두 객체를 비교하여 두 값이 모두 동일하면 TRUE 값을, 그렇지 않으면 FALSE 값을 반환.
+identical(extracted_row,extracted_row_in)
+
+#select()를 활용하면 데이터의 원하는 속성값만을 선택하여 추출할 수 가 있다.
+extracted_columns<-select(extracted_row, 계절, 일일회원)
+extracted_columns
+
+#mutate()를 활용하여 계산된 속성을 기존 데이터에 삽입할 수 가 있다.
+add_revenue<-mutate(extracted_columns, 수익 = 일일회원 * 1500)
+add_revenue
+
+#dplyr 패키지의 group_by()와 summarise() 함수로 데이터를 그룹별로 합계할 수 있다.
+# 두 함수는 같이 사용되는 경우가 많으니 메모해 두면 좋다.
+#group_by()는 데이터 프레임과 기준으로 만들고 싶은 변수를 파라미터로 취한다.
+grouped <-group_by(add_revenue,계절)
+
+#summarise()는 데이터프레임과 사용자가 요약하길 원하는 변수를 파라미터로 여긴다.
+#평균, 최소, 최대, 총합 중 정확하게 선택하여야 합니다.
+report<-summarise(grouped, sum(일일회원), sum(수익),mean(수익))
+report
+
+#데이터를 csv 파일로 저장하는 방법
+#write.csv()함수는 데이터 프레임을 csv파일로 바꿔 준다.
+#마지막 파라미터의 FALSE 값은 분석 결과의 행 번호가 함께 저장하지 않는다는 의미 입니다.
+write.csv(report,"수익_리포트.csv",row.names = FALSE)
+#write.table()함수도 마찬가지로 데이터프레임을 txt 파일로 저장할 수 있는 기능을 제공합니다. 
+#이때 sep = 파라미터의 '\t'는 텍스트 파일을 저장할 때, 텝(tap)으로 구분하라고 알려주는 겁니다.
+write.table(report,"수익_리포트.txt",row.names = FALSE, sep = "\t")
+
---
+++ R_Business/Ch_02_데이터처리.R
@@ -0,0 +1,125 @@
+#CH_02 데이터 처리하기 
+getwd()
+setwd("E:/hello-git-sourcetree/R_GO/R_Business")
+
+#데이터 불러 들어 kickboard라는 데이터 프레임으로 불러들입니다.
+kickboard<-read.csv("Ch2_kickboard_sharing_data.csv",stringsAsFactors = FALSE)
+
+#str 함수를 이용해 데이터 확인
+str(kickboard)
+#이외에도 다양한 함수를 통해 데이터를 간략하게 요약하여 확인할 수 가 있다.
+dim(kickboard) # 데이터프레임의 행과 열의 차원을 보여줍니다.
+head(kickboard) # 데이터프레임의 처음 6개 행과 모든 속성을 보여 줍니다. 
+tail(kickboard) # 데이터프레임의 마지막 6개 행과 모든 속성을 보여 줍니다.
+
+#결함 데이터 찾아 수정하기
+#is.na 는 "NA 값이다." 라는 것을 의미한다.
+#이 함수를 수행하면 NA만큼 TRUE를 반환 합니다. 
+table(is.na(kickboard)) 
+
+#stringr 패키지 안에 str_detect() 함수를 사용하면 
+#데이터프레임의 모든 NA 값을 찾을 수 가 있습니다.
+#이 함수를 수행하면 속성 마다 NA 값이 존재할 때 TRUE값을 반환합니다. 
+library(stringr)
+str_detect(kickboard,"NA")
+
+#기온 속성값에 하나의 NA값이 있음을 확인할 수 있습니다.
+table(is.na(kickboard$기온..C.))
+#풍속 속성값에 두개의 NA값이 있음을 확인할 수 있습니다.
+table(is.na(kickboard$풍속.m.s.))
+
+#!complete.cases()를 통해 NA값을 가지고 있는 행을 전부 출력할 수가 있다.
+kickboard_NA<-kickboard[!complete.cases(kickboard),]
+
+#첫번째 결측치 처리 방법_제외
+#아래 함수는 결측지가 존재하는 값을 제외하고 나머지 행만을 출력합니다.
+na.omit(kickboard)
+na.exclude(kickboard)
+
+#두번째 결측치 처리 방법_0 치환
+#아래 함수를 통해 NA값을 0으로 치환하여 데이터의 결측치를 제거할 수 있습니다.
+#치환은 어떠한 값을 어떻게 수정할지 충분히 고민 한 후 이행해야 합니다. 
+kickboard$적설.cm.[is.na(kickboard$적설.cm.)] <- 0
+table(is.na(kickboard$적설.cm.)) #확인
+
+#세번째 결측치 처리 방법_평균 치환
+#아래 함수를 통해 NA값을 평균으로 치환하여 데이터의 결측치를 제거할 수 있습니다.
+#이 외에도 중앙값, 최빈값을 사용할 수 가 있습니다.
+kickboard[!complete.cases(kickboard$기온..C.),] #현재 기온의 결측값 탐색 
+
+#기온에 대한 전체 평균값으로 치환 
+kickboard$기온..C.[is.na(kickboard$기온..C.)]<-mean(kickboard$기온..C., na.rm = TRUE)
+kickboard[7986,] #확인
+
+#가을 기온에 대한 전체 평균값으로 치환 
+#filter 함수를 통해 기온 데이터만 따로 추출합니다.
+kickboard_fall <-filter(kickboard ,계절 == 3, 기온..C.)
+#추출된 데이터를 가지고 평균을내어 값을 저장합니다.
+kickboard_fall_mean<-mean(kickboard_fall$기온..C.)
+
+#평균값을 데이터에 넣어주기 
+kickboard[7986,6]<-kickboard_fall_mean
+
+#풍속 결측치 평균으로 처리하기
+kickboard[!complete.cases(kickboard$풍속.m.s.),]
+kickboard_wind = filter(kickboard,계절 == 3, 풍속.m.s.)
+kickboard_wind_mean<-mean(kickboard_wind$풍속.m.s.)
+
+kickboard[8381,7]<-kickboard_wind_mean
+kickboard[8382,7]<-kickboard_wind_mean
+
+#CH2-2 분석 목적에 맞도록 데이터 형태 가공하기
+kickboard$휴일여부 <-factor(kickboard$휴일여부, levels = c(0,1),
+                        labels = c("아니요","네"))
+
+kickboard$근무여부 <-factor(kickboard$근무여부, levels = c(0,1),
+                        labels = c("아니요","네"))
+
+kickboard$계절 <-factor(kickboard$계절, levels = c(1,2,3,4),
+                      labels = c("봄","여름","가을","겨울"),
+                      ordered = TRUE)
+
+kickboard$날씨상황 <- factor(kickboard$날씨상황, levels =c(1,2,3,4),
+                         labels = c("맑음","흐림","약한 눈 비","강한 눈 비"),
+                         ordered = TRUE)
+str(kickboard)
+
+#날짜 및 시간 변환 
+#strptime 함수를 활용하여 문자열 형식에서 날짜형식으로 변환 
+#format 설정을 상세하게 해주지 않으면 NA값을 출력합니다.
+#Y를 대문자로 설정하면 4자리 년도, 소문자로 설정하면 2자리 년도 출력
+kickboard$날짜<-strptime(kickboard$날짜, format = "%Y-%m-%d %H:%M")
+
+str(kickboard)
+
+# 문자열 데이터를 표준형으로 가공하기 
+# 변수 확인  
+unique(kickboard$경로)
+kickboard$경로 <- tolower(kickboard$경로) #소문자 변환 
+kickboard$경로 <- str_trim(kickboard$경로) #앞 뒤 공백 제거
+na_loc<-is.na(kickboard$경로) # 경로의 모든 na값을 na_loc에 저장
+kickboard$경로[na_loc] <-"unknown" # 경로의 na값을 unknown 으로 대체
+
+#확인
+unique(kickboard$경로)
+
+#속성 간소화 하기 
+library(DataCombine)
+web_sites<- "(www.[a-z]*.[a-z]*)"
+#str_subset()는 string중 해당 패턴을 가지고 있는 문자열들을 반환합니다.
+current<-unique(str_subset(kickboard$경로, web_sites))
+current
+#rep()는 첫번째 인자를 두번째 인자만큼 반복
+replace<-rep("web",length(current))
+replace
+replacements<-data.frame(from = current,to = replace)
+replacements
+
+#
+kickboard<-FindReplace(data = kickboard, Var = "경로", replacements, from = "from", to = "to",exact = FALSE)
+
+unique(kickboard$경로)
+
+kickboard$경로<-as.factor(kickboard$경로)
+
+str(kickboard)
