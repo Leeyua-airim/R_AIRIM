ba811700d5922254953c547e5dc1f417aed55258
Leeyua-airim <brink9205@gmail.com>
2019-12-12 23:57:45 +0900

    05_korea_restaurants Data_mining

--- R_BigData_Analysis/Ch_05_DataMining.R
+++ R_BigData_Analysis/Ch_05_DataMining.R
@@ -1,4 +1,18 @@
 getwd()
 setwd("E:/hello-git-sourcetree/R_GO/R_BigData_Analysis")
 stations<-read.csv("Ch5_bike_station_locations.csv")
+restaurants<-read.csv("Ch5_Korea_restaurants.csv")
 summary(stations)
+
+hist(stations$경도, ylim = c(0,60),col = "gray")
+
+hist(restaurants$위도,col = "gray")
+hist(restaurants$경도, ylim = c(0,80),col = "gray")
+plot(restaurants$경도, restaurants$위도, asp = 1)
+
+set.seed(123)
+two<-kmeans(restaurants,2)
+three<-kmeans(restaurants,3)
+
+two
+three
